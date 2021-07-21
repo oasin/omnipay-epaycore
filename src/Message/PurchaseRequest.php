458621bef5351bb6e739f2dca139ab7084c83454
oasin <31675779+oasin@users.noreply.github.com>
2021-07-21 11:09:16 +0100

    Uploads

---
+++ src/Message/PurchaseRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Omnipay\EpayCore\Message;
+
+use Omnipay\Common\Exception\InvalidRequestException;
+
+class PurchaseRequest extends AbstractRequest
+{
+    /**
+     * @return mixed
+     * @throws InvalidRequestException
+     */
+    public function getData()
+    {
+        // Validate required parameters before return data
+        $this->validate('merchant_id', 'passphrase', 'currency', 'amount');
+
+        $data['epc_merchant_id'] = $this->getMerchantID();
+        $data['epc_amount'] = $this->getAmount();
+        $data['epc_currency_code'] = $this->getCurrency(); // USD, EUR or OAU
+        $data['epc_order_id'] = $this->getTransactionId();
+        $data['epc_status_url'] = $this->getNotifyUrl();
+        $data['epc_success_url	'] = $this->getReturnUrl();
+        $data['epc_cancel_url	'] = $this->getCancelUrl();
+        $data['epc_descr'] = $this->getDescription();
+
+        $sign = [
+            $data['epc_merchant_id'],
+            $data['epc_amount'],
+            $data['epc_currency_code'],
+            $data['epc_order_id'],
+            $this->getPassphrase() // merchant password
+        ];
+
+        # get epc_sign hash
+        $sign = hash('sha256', implode(':', $sign));
+
+
+        $data['epc_sign'] = $sign;
+
+        return $data;
+    }
+
+    public function sendData($data)
+    {
+        return new PurchaseResponse($this, $data, $this->getEndpoint());
+    }
+}
